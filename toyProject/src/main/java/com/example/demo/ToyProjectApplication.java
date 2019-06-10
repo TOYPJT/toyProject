e99fb2fdde140b8c10e772c658036fe0e1255151
TOYPJT <51563084+TOYPJT@users.noreply.github.com>
2019-06-10 12:50:48 +0900

    테스트

--- toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
+++ toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToyProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("½ÇÇà");
 		SpringApplication.run(ToyProjectApplication.class, args);
 	}
 
