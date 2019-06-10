90b756e55a9ab58727c6b18ecbe978ed6b1f40e8
papakang22 <papakang22@naver.com>
2019-06-10 16:40:39 +0900

    war버전

--- toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
+++ toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ToyProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("실행");
 		SpringApplication.run(ToyProjectApplication.class, args);
 	}
 
