e3b9b95ce217bb633f929b09ca0c93c604f51f46
TOYPJT <51563084+TOYPJT@users.noreply.github.com>
2019-06-11 09:36:41 +0900

    컨트롤러 위치변경

--- toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
+++ toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan
 public class ToyProjectApplication {
 
 	public static void main(String[] args) {
