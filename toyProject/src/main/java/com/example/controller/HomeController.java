441d923fc9c820fbcbfdd2f7297ec9c8a5178b73
TOYPJT <51563084+TOYPJT@users.noreply.github.com>
2019-06-11 09:21:18 +0900

    컨트롤러 테스트

--- toyProject/src/main/java/com/example/controller/HomeController.java
+++ toyProject/src/main/java/com/example/controller/HomeController.java
@@ -2,16 +2,19 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
 	
 	@RequestMapping(value = "/")
+	@ResponseBody
 	public String home() {
-		System.out.println("ì»¨íŠ¸ë¡¤ëŸ¬ ");
+		System.out.println("ÄÁÆ®·Ñ·¯");
 		return "home";
 	}
 	
+
 	
 }
--- toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
+++ toyProject/src/main/java/com/example/demo/ToyProjectApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,5 +13,7 @@ public class ToyProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToyProjectApplication.class, args);
 	}
+	
+
 
 }
