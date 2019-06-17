00ed9eaa8d9fb82570bbff556c36ab4f5fe4118f
TOYPJT <51563084+TOYPJT@users.noreply.github.com>
2019-06-17 11:06:33 +0900

    임시 서비스 컨트롤러 파일 수정

--- toyProject/src/main/java/com/example/demo/controller/HomeController.java
+++ toyProject/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.example.demo.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.service.TmpService;
+import com.example.demo.tmp_entity.TmpEntity;;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+    private TmpService tmpService;
 
 	@RequestMapping("/")
 	public String goMain(Model model){
+		List<TmpEntity> list = tmpService.getAllTmpMember();
+	    model.addAttribute("list", list);
 		return "index";
 	}
 	
