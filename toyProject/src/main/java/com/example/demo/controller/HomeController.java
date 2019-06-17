baf09818dd743776d55c33f2116ee14d810c4e5a
papakang22 <papakang22@naver.com>
2019-06-17 12:19:40 +0900

    에러수정

--- toyProject/src/main/java/com/example/demo/controller/HomeController.java
+++ toyProject/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,16 +1,28 @@
 package com.example.demo.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.service.TmpService;
+import com.example.demo.tmp_entity.TmpEntity;;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+    private TmpService tmpService;
 
 	@RequestMapping("/")
 	public String goMain(Model model){
-		return "index";
+		List<TmpEntity> list = tmpService.getAllTmpMember();
+	    model.addAttribute("list", list);
+		return "main/index";
 	}
+	
 	
 }
